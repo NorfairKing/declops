664ab9537796797548331f3ebf462aee363f78af
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-05-28 14:47:23 +0200

    refactor the temp file provider

--- declops-provider-local/src/Declops/Provider/Local/TempFile.hs
+++ declops-provider-local/src/Declops/Provider/Local/TempFile.hs
@@ -60,81 +60,97 @@ tempFileProvider :: Provider TempFileSpecification (Path Abs File) TempFileOutpu
 tempFileProvider =
   Provider
     { providerName = "temporary-file",
-      providerQuery = \reference -> do
-        mContents <- forgivingAbsence $ TIO.readFile $ fromAbsFile reference
-        pure $ case mContents of
-          Nothing -> DoesNotExistRemotely
-          Just contents ->
-            ExistsRemotely
-              TempFileOutput
-                { tempFileOutputPath = reference,
-                  tempFileOutputContents = contents
-                },
-      providerApply = \TempFileSpecification {..} applyContext -> do
-        case applyContext of
-          DoesNotExistLocallyNorRemotely -> do
-            (tfile, handle) <- openTempFile tempFileSpecificationBase (T.unpack tempFileSpecificationTemplate)
-            TIO.hPutStr handle tempFileSpecificationContents
-            hClose handle
-            let output = TempFileOutput {tempFileOutputPath = tfile, tempFileOutputContents = tempFileSpecificationContents}
-            pure $ ApplySuccess tfile output
-          ExistsLocallyButNotRemotely reference -> do
-            ignoringAbsence $ removeFile reference
-            (tfile, handle) <- openTempFile tempFileSpecificationBase (T.unpack tempFileSpecificationTemplate)
-            TIO.hPutStr handle tempFileSpecificationContents
-            hClose handle
-            let output = TempFileOutput {tempFileOutputPath = tfile, tempFileOutputContents = tempFileSpecificationContents}
-            pure $ ApplySuccess tfile output
-          ExistsLocallyAndRemotely reference output@TempFileOutput {..} -> do
-            let alreadyCorrect =
-                  and
-                    [ case stripProperPrefix tempFileSpecificationBase tempFileOutputPath of
-                        Nothing -> False
-                        Just subfile -> T.unpack tempFileSpecificationTemplate `isInfixOf` fromRelFile subfile,
-                      tempFileOutputContents == tempFileSpecificationContents
-                    ]
-            if alreadyCorrect
-              then pure $ ApplySuccess reference output
-              else do
-                ignoringAbsence $ removeFile reference
-                (tfile, handle) <- openTempFile tempFileSpecificationBase (T.unpack tempFileSpecificationTemplate)
-                TIO.hPutStr handle tempFileSpecificationContents
-                hClose handle
-                let newOutput = TempFileOutput {tempFileOutputPath = tfile, tempFileOutputContents = tempFileSpecificationContents}
-                pure $ ApplySuccess tfile newOutput,
-      providerCheck = \TempFileSpecification {..} reference -> do
-        case stripProperPrefix tempFileSpecificationBase reference of
-          Nothing -> pure $ CheckFailure "File had the wrong base."
-          Just subfile ->
-            if T.unpack tempFileSpecificationTemplate `isInfixOf` fromRelFile subfile
-              then do
-                mContents <- forgivingAbsence $ TIO.readFile $ fromAbsFile reference
-                pure $ case mContents of
-                  Nothing -> CheckFailure "File does not exist."
-                  Just contents ->
-                    if contents == tempFileSpecificationContents
-                      then
-                        CheckSuccess
-                          TempFileOutput
-                            { tempFileOutputPath = reference,
-                              tempFileOutputContents = contents
-                            }
-                      else
-                        CheckFailure $
-                          unlines
-                            [ "File did not have the right contents:",
-                              unwords ["expected:", show tempFileSpecificationContents],
-                              unwords ["actual:  ", show contents]
-                            ]
-              else
-                pure $
+      providerQuery = queryTempFile,
+      providerApply = applyTempFile,
+      providerCheck = checkTempFile,
+      providerDestroy = destroyTempFile
+    }
+
+queryTempFile :: Path Abs File -> IO (RemoteState TempFileOutput)
+queryTempFile path = do
+  mContents <- forgivingAbsence $ TIO.readFile $ fromAbsFile path
+  pure $ case mContents of
+    Nothing -> DoesNotExistRemotely
+    Just contents ->
+      ExistsRemotely
+        TempFileOutput
+          { tempFileOutputPath = path,
+            tempFileOutputContents = contents
+          }
+
+applyTempFile :: TempFileSpecification -> ApplyContext (Path Abs File) TempFileOutput -> IO (ApplyResult (Path Abs File) TempFileOutput)
+applyTempFile TempFileSpecification {..} applyContext = do
+  case applyContext of
+    DoesNotExistLocallyNorRemotely -> do
+      tfile <- makeTempFile tempFileSpecificationBase tempFileSpecificationTemplate tempFileSpecificationContents
+      let output = TempFileOutput {tempFileOutputPath = tfile, tempFileOutputContents = tempFileSpecificationContents}
+      pure $ ApplySuccess tfile output
+    ExistsLocallyButNotRemotely reference -> do
+      removeTempFile reference
+      tfile <- makeTempFile tempFileSpecificationBase tempFileSpecificationTemplate tempFileSpecificationContents
+      let output = TempFileOutput {tempFileOutputPath = tfile, tempFileOutputContents = tempFileSpecificationContents}
+      pure $ ApplySuccess tfile output
+    ExistsLocallyAndRemotely reference output@TempFileOutput {..} -> do
+      let alreadyCorrect =
+            and
+              [ case stripProperPrefix tempFileSpecificationBase tempFileOutputPath of
+                  Nothing -> False
+                  Just subfile -> T.unpack tempFileSpecificationTemplate `isInfixOf` fromRelFile subfile,
+                tempFileOutputContents == tempFileSpecificationContents
+              ]
+      if alreadyCorrect
+        then pure $ ApplySuccess reference output
+        else do
+          removeTempFile reference
+          tfile <- makeTempFile tempFileSpecificationBase tempFileSpecificationTemplate tempFileSpecificationContents
+          let newOutput = TempFileOutput {tempFileOutputPath = tfile, tempFileOutputContents = tempFileSpecificationContents}
+          pure $ ApplySuccess tfile newOutput
+
+checkTempFile :: TempFileSpecification -> Path Abs File -> IO (CheckResult TempFileOutput)
+checkTempFile TempFileSpecification {..} path = do
+  case stripProperPrefix tempFileSpecificationBase path of
+    Nothing -> pure $ CheckFailure "File had the wrong base."
+    Just subfile ->
+      if T.unpack tempFileSpecificationTemplate `isInfixOf` fromRelFile subfile
+        then do
+          mContents <- forgivingAbsence $ TIO.readFile $ fromAbsFile path
+          pure $ case mContents of
+            Nothing -> CheckFailure "File does not exist."
+            Just contents ->
+              if contents == tempFileSpecificationContents
+                then
+                  CheckSuccess
+                    TempFileOutput
+                      { tempFileOutputPath = path,
+                        tempFileOutputContents = contents
+                      }
+                else
                   CheckFailure $
                     unlines
-                      [ "File did not have the right template:",
-                        unwords ["expected:   ", T.unpack tempFileSpecificationTemplate],
-                        unwords ["actual file:", fromAbsFile reference]
-                      ],
-      providerDestroy = \reference -> do
-        ignoringAbsence $ removeFile reference
-        pure DestroySuccess
-    }
+                      [ "File did not have the right contents:",
+                        unwords ["expected:", show tempFileSpecificationContents],
+                        unwords ["actual:  ", show contents]
+                      ]
+        else
+          pure $
+            CheckFailure $
+              unlines
+                [ "File did not have the right template:",
+                  unwords ["expected:   ", T.unpack tempFileSpecificationTemplate],
+                  unwords ["actual file:", fromAbsFile path]
+                ]
+
+destroyTempFile :: Path Abs File -> IO DestroyResult
+destroyTempFile path = do
+  removeTempFile path
+  pure DestroySuccess
+
+removeTempFile :: Path Abs File -> IO ()
+removeTempFile path = ignoringAbsence $ removeFile path
+
+makeTempFile :: Path Abs Dir -> Text -> Text -> IO (Path Abs File)
+makeTempFile base template contents = do
+  (tfile, handle) <- openTempFile base (T.unpack template)
+  TIO.hPutStr handle contents
+  hClose handle
+  pure tfile
